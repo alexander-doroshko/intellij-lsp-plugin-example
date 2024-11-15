575fbcadc2127908f26862620b2b16bd4ab3ab1d
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2024-11-15 19:29:59 +0100

    Let LspServerWidgetItem have a technology-specific icon

--- src/main/kotlin/com/lsp_plugin_example/DartLspServerSupportProvider.kt
+++ src/main/kotlin/com/lsp_plugin_example/DartLspServerSupportProvider.kt
@@ -2,8 +2,10 @@ package com.lsp_plugin_example
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.platform.lsp.api.LspServer
 import com.intellij.platform.lsp.api.LspServerSupportProvider
 import com.intellij.platform.lsp.api.LspServerSupportProvider.LspServerStarter
+import com.intellij.platform.lsp.api.lsWidget.LspServerWidgetItem
 
 class DartLspServerSupportProvider : LspServerSupportProvider {
   override fun fileOpened(project: Project, file: VirtualFile, serverStarter: LspServerStarter) {
@@ -11,4 +13,7 @@ class DartLspServerSupportProvider : LspServerSupportProvider {
       serverStarter.ensureServerStarted(DartLspServerDescriptor(project))
     }
   }
+
+  override fun createLspServerWidgetItem(lspServer: LspServer, currentFile: VirtualFile?): LspServerWidgetItem? =
+    LspServerWidgetItem(lspServer, currentFile, icon = DartIcons.Dart, settingsPageClass = null)
 }
